07f521f033ffd0e1e52665efe7c40ba13ba45223
blue7un <level.newbie@gmail.com>
2015-06-28 23:59:15 +0700

    Add new type of Jump

---
+++ JumpGame/Assets/JumpAndJump/scripts/gui/PressButton.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressButton : UIEventTrigger {
+    public GameObject Player;   
+
+    float pressTime = -1f;
+    float tressHold = 0.5f; // tressHold = 0.5 seconds
+    bool buttonIsPressed = false;
+
+    void OnPress(bool isPressed)
+    {
+        if (isPressed)
+        {
+            if (pressTime < 0)
+                pressTime = Time.time;
+
+            buttonIsPressed = true;
+            StartCoroutine("checkHoldButton");
+        }
+        else
+        {
+            float deltaTime = Time.time - pressTime;
+            if (deltaTime < tressHold)
+            {
+                buttonIsPressed = false;
+                pressTime = -1f;
+                Player.GetComponent<Player>().JumpNear();
+            }
+        }
+    }
+
+    IEnumerator checkHoldButton() {
+        while (buttonIsPressed) {
+            if (Time.time - pressTime > tressHold)
+            {
+                buttonIsPressed = false;
+                pressTime = -1f;
+                Player.GetComponent<Player>().JumpFar();
+            }
+
+            yield return new WaitForSeconds(0.05f);
+        }    
+    }
+}
